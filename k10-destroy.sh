918c91d503144a5e7f1f9c5283b83fae8059b7fd
Yongkang He <he@yongkang.cloud>
2021-11-19 14:39:27 +0800

    Create k10-destroy.sh

---
+++ k10-destroy.sh
@@ -0,0 +1,24 @@
+starttime=$(date +%s)
+. setenv.sh
+# MY_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print tolower($0)}')
+
+echo "-------Delete postgresql & kasten-io"
+helm uninstall postgres -n k10-postgresql
+helm uninstall k10 -n kasten-io
+kubectl delete ns k10-postgresql
+kubectl delete ns kasten-io
+
+echo '-------Deleting objects from the bucket'
+aws s3 rb s3://$(cat hcp_bucketname) --force
+
+# echo '-------Deleting kubeconfig for this cluster'
+# kubectl config delete-context $(kubectl config get-contexts | grep $MY_PREFIX-$MY_CLUSTER | awk '{print $2}')
+
+echo "" | awk '{print $1}'
+endtime=$(date +%s)
+duration=$(( $endtime - $starttime ))
+echo "-------Total time is $(($duration / 60)) minutes $(($duration % 60)) seconds."
+echo "" | awk '{print $1}'
+echo "-------Created by Yongkang"
+echo "-------Email me if any suggestions or issues he@yongkang.cloud"
+echo "" | awk '{print $1}'
