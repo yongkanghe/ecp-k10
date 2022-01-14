29da9eca3d0c125c9b10ad7bba490396f16922ac
Yongkang He <he@yongkang.cloud>
2022-01-14 09:43:06 +0800

    Update k10-deploy.sh

--- k10-deploy.sh
+++ k10-deploy.sh
@@ -170,22 +170,22 @@ spec:
           - k10-postgresql
 EOF
 
-sleep 7
+# sleep 7
 
-echo '-------Kickoff the on-demand backup job'
-sleep 8
+# echo '-------Kickoff the on-demand backup job'
+# sleep 8
 
-cat <<EOF | kubectl create -f -
-apiVersion: actions.kio.kasten.io/v1alpha1
-kind: RunAction
-metadata:
-  generateName: run-backup-
-spec:
-  subject:
-    kind: Policy
-    name: k10-postgresql-backup
-    namespace: kasten-io
-EOF
+# cat <<EOF | kubectl create -f -
+# apiVersion: actions.kio.kasten.io/v1alpha1
+# kind: RunAction
+# metadata:
+#   generateName: run-backup-
+# spec:
+#   subject:
+#     kind: Policy
+#     name: k10-postgresql-backup
+#     namespace: kasten-io
+# EOF
 
 echo '-------Accessing K10 UI'
 cat ecp-token
